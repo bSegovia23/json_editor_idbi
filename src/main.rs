9f226629716d36463e2686d845007b3a436da18d
bSegovia23 <segovia@usc.edu>
2023-08-01 12:20:21 -0400

    It works!

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,86 @@
 use std::fs::File;
 use std::io::prelude::*;
 use serde::{Deserialize, Serialize};
-use eframe::{egui, epi};
+use eframe::egui;
 use egui_extras;
 use chrono::NaiveDate;
+
+const APP_NAME: &str = "JSON Editor";
+
+struct MyApp {
+    json_data: JsonData,
+}
+
+impl MyApp {
+    fn new(cc: &eframe::CreationContext<'_>) -> Self {
+        // If you want to customize GUI fonts and visuals, do it here.
+        // cc.egui_ctx.set_fonts and cc.egui_ctx.set_visuals
+        Self::default()
+    }
+}
+
+impl Default for MyApp {
+    fn default() -> Self {
+        // Load the initial JSON data from a file
+        let json_data = match load_json_data() {
+            Ok(data) => data,
+            Err(e) => {
+                println!("Error loading JSON data: {}", e);
+                JsonData::default()
+            }
+        };
+
+        MyApp { json_data }
+    }
+}
+
+impl eframe::App for MyApp {
+    fn update(&mut self, ctx: &egui::Context, _frame: &mut eframe::Frame) {
+        // UI code goes here
+        egui::CentralPanel::default().show(ctx, |ui| {
+            // ui.heading();
+
+            // Create UI elements to edit your JSON data
+            ui.horizontal(|ui| {
+                ui.label("Number of Stages:");
+                ui.add(egui::widgets::Slider::new(&mut self.json_data.n_stages, 1..=4));
+            });
+
+            ui.horizontal(|ui| {
+                ui.label("Step Size in Months:");
+                ui.add(egui::widgets::Slider::new(&mut self.json_data.step_size_months, 1..=6));
+            });
+
+            ui.horizontal(|ui| {
+                ui.label("Base Date:");
+                ui.add(egui_extras::DatePickerButton::new(&mut self.json_data.base_date));
+            });
+            
+            let environment_options = [
+                Environment::Production,
+                Environment::Development,
+                Environment::Testing,
+            ];
+
+            ui.horizontal(|ui| {
+                ui.label("Environment:");
+                for option in environment_options {
+                    ui.radio_value(&mut self.json_data.environment, option, option.to_user_friendly_label());
+                }
+            });
+
+            // Add more UI elements as needed for other fields
+
+            if ui.button("Save").clicked() {
+                // Save the JSON data when the "Save" button is clicked
+                match save_json_data(&self.json_data) {
+                    Ok(_) => println!("Data saved successfully!"),
+                    Err(e) => println!("Error saving data: {}", e),
+                }
+            }
+        });
+    }
+}
 
 #[derive(Debug, Deserialize, Serialize)]
 struct JsonData {
@@ -50,25 +127,6 @@ impl Environment {
     }
 }
 
-struct MyApp {
-    json_data: JsonData,
-}
-
-impl Default for MyApp {
-    fn default() -> Self {
-        // Load the initial JSON data from a file
-        let json_data = match load_json_data() {
-            Ok(data) => data,
-            Err(e) => {
-                println!("Error loading JSON data: {}", e);
-                JsonData::default()
-            }
-        };
-
-        MyApp { json_data }
-    }
-}
-
 fn load_json_data() -> Result<JsonData, Box<dyn std::error::Error>> {
     // Read the JSON data from a file (change "data.json" to your file name)
     let mut file = File::open("data.json")?;
@@ -92,62 +150,12 @@ fn save_json_data(data: &JsonData) -> Result<(), Box<dyn std::error::Error>> {
     Ok(())
 }
 
-impl epi::App for MyApp {
-    fn name(&self) -> &str {
-        "JSON Editor" // Provide a name for your application
-    }
-    fn update(&mut self, ctx: &egui::CtxRef, _frame: &mut epi::Frame) {
-        // UI code goes here
-        egui::CentralPanel::default().show(ctx, |ui| {
-            ui.heading(self.name());
-
-            // Create UI elements to edit your JSON data
-            ui.horizontal(|ui| {
-                ui.label("Number of Stages:");
-                ui.add(egui::widgets::Slider::new(&mut self.json_data.n_stages, 1..=4));
-            });
-
-            ui.horizontal(|ui| {
-                ui.label("Step Size in Months:");
-                ui.add(egui::widgets::Slider::new(&mut self.json_data.step_size_months, 1..=6));
-            });
-
-            ui.horizontal(|ui| {
-                ui.label("Base Date:");
-                ui.add(egui_extras::DatePickerButton::new(&mut self.json_data.base_date));
-            });
-            
-            let environment_options = [
-                Environment::Production,
-                Environment::Development,
-                Environment::Testing,
-            ];
-
-            ui.horizontal(|ui| {
-                ui.label("Environment:");
-                for option in environment_options {
-                    ui.radio_value(&mut self.json_data.environment, option, option.to_user_friendly_label());
-                }
-            });
-
-            // Add more UI elements as needed for other fields
-
-            if ui.button("Save").clicked() {
-                // Save the JSON data when the "Save" button is clicked
-                match save_json_data(&self.json_data) {
-                    Ok(_) => println!("Data saved successfully!"),
-                    Err(e) => println!("Error saving data: {}", e),
-                }
-            }
-        });
-    }
-}
-
 fn main() {
     let app = MyApp::default();
-    let options = eframe::NativeOptions {
-        initial_window_size: Some(egui::vec2(400.0, 200.0)),
-        ..Default::default()
-    };
-    eframe::run_native(Box::new(app), options);
+    // let options = eframe::NativeOptions {
+    //     initial_window_size: Some(egui::vec2(400.0, 200.0)),
+    //     ..Default::default()
+    // };
+    let options = eframe::NativeOptions::default();
+    eframe::run_native(APP_NAME, options, Box::new(|cc| Box::new(MyApp::new(cc))));
 }
